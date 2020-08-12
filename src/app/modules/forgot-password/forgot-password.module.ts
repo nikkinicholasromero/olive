ef163668659ad2cbbc3c4af952718b5ae1c29807
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-08-12 04:04:22 -0400

    Used shared form-field-component in forgot-password-component

--- src/app/modules/forgot-password/forgot-password.module.ts
+++ src/app/modules/forgot-password/forgot-password.module.ts
@@ -1,14 +1,16 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared.module';
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
 import { ForgotPasswordComponent } from './forgot-password.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ForgotPasswordRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule,
+    ForgotPasswordRoutingModule
   ],
   declarations: [ForgotPasswordComponent]
 })

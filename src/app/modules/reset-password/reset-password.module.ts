ab7d16d92a4b8895a87481f9944ab9fd9406f1b4
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-08-12 04:06:27 -0400

    Used shared form-field-component in reset-password-component

--- src/app/modules/reset-password/reset-password.module.ts
+++ src/app/modules/reset-password/reset-password.module.ts
@@ -1,14 +1,16 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared.module';
 import { ResetPasswordRoutingModule } from './reset-password-routing.module';
 import { ResetPasswordComponent } from './reset-password.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ResetPasswordRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule,
+    ResetPasswordRoutingModule
   ],
   declarations: [ResetPasswordComponent]
 })

d0691c1a5f8f0dd4710ac0c744cae221b80f0019
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-08-17 08:05:34 +0800

    Created Cypress test for Forgot Password page

---
+++ e2e/cypress/integration/forgot-password/forgot-password.spec.js
@@ -0,0 +1,53 @@
+describe("Forgot Password page", () => {
+    it("should validate user input", () => {
+        cy.visit("http://localhost:4200/forgotPassword");
+
+        cy.get("#formTitle")
+            .should("visible")
+            .should("contain", "Olive | Forgot Password");
+
+        cy.get("label[for='emailAddress']")
+            .should("visible")
+            .should("contain", "Email address");
+
+        cy.get("input#emailAddress")
+            .should("visible")
+            .should("have.attr", "type", "email")
+            .should("not.have.class", "error")
+            .should("value", "");
+
+        cy.get("label#emailAddressErrorMessage")
+            .should("not.visible");
+
+        cy.get("input#emailAddress")
+            .clear()
+            .type("abcdefghij")
+            .should("have.class", "error");
+
+        cy.get("label#emailAddressErrorMessage")
+            .should("visible")
+            .should("have.class", "error")
+            .should("contain", "Please enter a valid email address");
+
+        cy.get("input#emailAddress")
+            .clear()
+            .should("have.class", "error");
+
+        cy.get("label#emailAddressErrorMessage")
+            .should("visible")
+            .should("have.class", "error")
+            .should("contain", "Email address is required");
+
+        cy.get("input#emailAddress")
+            .type("valid@email")
+            .should("not.have.class", "error");
+
+        cy.get("label#emailAddressErrorMessage")
+            .should("not.visible");
+
+        cy.get("button[type='submit']")
+            .should("visible")
+            .should("have.attr", "type", "submit")
+            .should("contain", "Send Reset Link");
+    });
+});

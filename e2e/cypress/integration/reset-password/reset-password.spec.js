9a98a4cfffca4f3b785ff81b3345d1f973085a28
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-08-17 08:23:07 +0800

    Created Cypress test for Reset Password page

---
+++ e2e/cypress/integration/reset-password/reset-password.spec.js
@@ -0,0 +1,71 @@
+describe("Reset Password page", () => {
+    it("should validate user input", () => {
+        cy.visit("http://localhost:4200/resetPassword");
+
+        cy.get("#formTitle")
+            .should("visible")
+            .should("contain", "Olive | Reset Password");
+
+        cy.get("label[for='password']")
+            .should("visible")
+            .should("contain", "New password");
+
+        cy.get("input#password")
+            .should("visible")
+            .should("have.attr", "type", "password")
+            .should("not.have.class", "error")
+            .should("value", "");
+
+        cy.get("label#passwordErrorMessage")
+            .should("not.visible");
+
+        cy.get("input#password")
+            .type("abcdefghij")
+            .clear()
+            .should("have.class", "error");
+
+        cy.get("label#passwordErrorMessage")
+            .should("visible")
+            .should("have.class", "error")
+            .should("contain", "New password is required");
+
+        cy.get("input#password")
+            .type("abcdefghij")
+            .should("not.have.class", "error");
+
+        cy.get("label#passwordErrorMessage")
+            .should("not.visible");
+
+        cy.get("label[for='confirmPassword']")
+            .should("visible")
+            .should("contain", "Confirm password");
+    
+        cy.get("input#confirmPassword")
+            .should("visible")
+            .should("have.attr", "type", "password")
+            .should("not.have.class", "error")
+            .should("value", "");
+
+        cy.get("input#confirmPassword")
+            .type("1234567890")
+            .clear()
+            .should("have.class", "error");
+
+        cy.get("label#confirmPasswordErrorMessage")
+            .should("visible")
+            .should("have.class", "error")
+            .should("contain", "Passwords does not match");
+
+        cy.get("input#confirmPassword")
+            .type("abcdefghij")
+            .should("not.have.class", "error");
+
+        cy.get("label#confirmPasswordErrorMessage")
+            .should("not.visible");
+
+        cy.get("button[type='submit']")
+            .should("visible")
+            .should("have.attr", "type", "submit")
+            .should("contain", "Reset Password");
+    });
+});

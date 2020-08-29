2d9cd482919a61e54ef1fced2f61a13b3689de77
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-08-29 09:34:28 +0800

    Added unit tests for ForgotPasswordComponent

--- karma.conf.js
+++ karma.conf.js
@@ -20,10 +20,10 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
-        statements: 80,
-        lines: 80,
-        branches: 80,
-        functions: 80
+        statements: 95,
+        lines: 95,
+        branches: 95,
+        functions: 95
       }
     },
     reporters: ['progress', 'kjhtml'],
